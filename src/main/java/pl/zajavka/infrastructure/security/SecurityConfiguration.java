4d9a8eec1beb8aef9b4ef4d29c9f33474bef11dd
Pawel9481 <pawel.demichowicz@interia.pl>
2025-06-02 20:00:27 +0200

    Update Spring Security configs

--- src/main/java/pl/zajavka/infrastructure/security/SecurityConfiguration.java
+++ src/main/java/pl/zajavka/infrastructure/security/SecurityConfiguration.java
@@ -40,7 +40,7 @@ public class SecurityConfiguration {
                         .requestMatchers("/mechanic/**").hasAnyAuthority("MECHANIC")
                         .requestMatchers("/salesman/**", "/purchase/**", "/service/**").hasAnyAuthority("SALESMAN")
                         .requestMatchers("/", "/car/**", "/images/**").hasAnyAuthority("MECHANIC", "SALESMAN")
-                        .requestMatchers("/api/**").hasAnyAuthority("REST_API")
+                        .requestMatchers("/**").hasAnyAuthority("REST_API")
                 )
                 .formLogin(withDefaults())
                 .logout(logout -> logout
